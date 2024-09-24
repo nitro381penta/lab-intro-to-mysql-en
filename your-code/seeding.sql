4855c8aa17796b862348a3e479d8986a45529e86
nitro381penta <151678037+nitro381penta@users.noreply.github.com>
2024-09-24 18:38:03 +0200

    Lab Day 12: Intro to MySQL
    Julia's solution

--- your-code/seeding.sql
+++ your-code/seeding.sql
@@ -1,32 +1,32 @@
-insert into cars(car_id, manufacturer, model, year_, color)
-values ('0', 'Volkswagen','Tiguan', '2019','Blue'),
-('1', 'Peugeot','Rifter', '2019', 'Red'),
-('2', 'Ford','Fusion', '2018', 'White'),
-('3', 'Toyota','RAV4', '2018', 'Silver'),
-('4', 'Volvo', 'V60', '2019', 'Gray'),
-('5', 'Volvo','V60 Cross Country', '2019', 'Gray');
-
-
-insert into costumer(costumer_id, first_name, phone_no, email, address, city, state, country, zip_code)
-values ('10001', 'Pablo Picasso', '+34 636 17 63 82', '-', 'Paseo de la Chope', 'Madrid', 'Madrid', 'Spain', '28045'),
-('20001', 'Abraham Lincoln', '+1 305 907 7086', '-', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
-('30001', 'Napoleon Bonaparte', '+33 1 79 75 40 00', '-', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
-
-
-insert into salesperson (salesperson_id, first_name, store_name)
-values ('00001', 'Petey Cruiser', '0'),
-('00002', 'Anna Sthesia', '1'),
-('00003', 'Paul Molive', '2'),
-('00004', 'Gail Forcewind', '3'),
-('00005', 'Paige Turner', '4'),
-('00006', 'Bob Frapples', '5'),
-('00007', 'Walter Melon', '6'),
-('00008', 'Shonda Leer', 'São Paulo');
-
-insert into invoices (invoice_id, date_, car, costumer, salesperson)
-values ('852399038', '2018-08-22',	'0','1','3'),
-('731166526', '2018-12-31', '3', '0', '5'),
-('271135104', '2019-01-22', '2','2','7');
-
-
-
+INSERT INTO CARS.cars (vehicle_identification_number, manufacturer, model, year, color, price)
+VALUES 
+    ('3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', 2019, 'Blue', 28999.99),
+    ('ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', 2019, 'Red', 21999.99),
+    ('RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', 2018, 'White', 24800.99),
+    ('HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', 2018, 'Silver', 22500.99),
+    ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60', 2019, 'Gray', 19999.99),
+    ('DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', 2019, 'Gray', 19999.99);
+    
+INSERT INTO CARS.costumers (name, phone_number, email, address, city, state_province, country, zip_postal_code)
+VALUES
+    ('Pablo Picasso', '+34 636 17 63 82', 'pablo.picasso@example.com', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+    ('Abraham Lincoln', '+1 305 907 7086', 'abraham.lincoln@example.com', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+    ('Napoléon Bonaparte', '+33 1 79 75 40 00', 'napoleon.bonaparte@example.com', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+    
+INSERT INTO CARS.salespersons (salesperson_id, name, store)
+VALUES
+    (1, 'Petey Cruiser', 'Madrid'),
+    (2, 'Anna Sthesia', 'Barcelona'),
+    (3, 'Paul Molive', 'Berlin'),
+    (4, 'Gail Forcewind', 'Paris'),
+    (5, 'Paige Turner', 'Mimia'),
+    (6, 'Bob Frapples', 'Mexico City'),
+    (7, 'Walter Melon', 'Amsterdam'),
+    (8, 'Shonda Leer', 'São Paulo');
+    
+    
+INSERT INTO CARS.invoices (invoice_id, date, car_id, customer_id, salesperson_id)
+VALUES
+    (852399038, '2018-08-22', 0, 1, 3),
+    (731166526, '2018-12-31', 3, 0, 5),
+    (271135104, '2019-01-22', 2, 2, 7);
